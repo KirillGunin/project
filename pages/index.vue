c0e716a6bfd485ded8e4dc5107d23780ba2f4de3
gunini <kg@webactives.ru>
2023-02-10 11:13:46 -0300

    components: postList, postPreview, correct styles

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
 <template>
-  <div class="wrapper-content wrapper-content--fixed"></div>
+  <div class="wrapper-content wrapper-content--fixed">
+    <PostsList :posts="posts"/>
+  </div>
 </template>
 
 <script>
+import PostsList from '~/components/Blog/PostsList.vue'
 export default {
+  components: { PostsList },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'lalka', description: 'bla bla', img: 'https://www.blessthisstuff.com/imagens/stuff/the-heritage-chief.jpg'},
+        {id: 2, title: 'lol', description: 'bla bla', img: 'https://www.fixie75.com/wp-content/uploads/2018/05/fixie75-state_bicycle_co_rutherford_3_blue_fixie_12-1.jpg'},
+        {id: 3, title: 'kek', description: 'bla bla', img: 'https://i.pinimg.com/originals/c0/1f/c2/c01fc2c00f539c5a2d5e50a827e36ce6.jpg'}
+      ]
+    }
+  }
 }
 </script>
